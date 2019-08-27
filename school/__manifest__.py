f69edce29f0d31725df28afff341ea0dfa81f3df
Chea Mengleng <cheamengleng@yahoo.com>
2019-08-27 08:05:20 +0000

    [ADD]add module school

---
+++ school/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+	'name': 'School',
+	'version': '12.0.1.0.0',
+	'summary': 'Record Student Information',
+	'category': 'Tools',
+	'author': 'Mengleng Chea',
+	'maintainer': 'Techno Solutions',
+	'company': 'Techno Solutions',
+	'website': 'https://www.cybrosys.com',
+	'depends': ['base'],
+	'data': [
+		'security/ir.model.access.csv',
+		'views/student_view.xml'
+	],
+	'images': [],
+	'license': 'AGPL-3',
+	'sequence': 1,
+	'installable': True,
+	'application': True,
+	'auto_install': False,
+}
